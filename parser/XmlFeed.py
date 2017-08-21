008995b5a24b4de354ab311bf16eb9bdfc4da563
crazyministr <crazyministr@gmail.com>
2017-08-21 22:04:33 +0300

    start project, parser version 1.0

---
+++ parser/XmlFeed.py
@@ -0,0 +1,83 @@
+try:
+    from parser.XmlParserBase import XmlParserBase
+except ImportError:
+    from XmlParserBase import XmlParserBase
+
+import xml.etree.ElementTree as ET
+
+
+class XmlFeed(XmlParserBase):
+    def __init__(self, source_file):
+        super().__init__()
+
+        tree = ET.parse(source_file, parser=ET.XMLParser(encoding='utf-8'))
+        root = tree.getroot()
+
+        for child in root:
+            self.xml_data[child.tag] = child
+
+    def parse_events(self, events):
+        for event in events:
+            self._events.append({
+                'uid': self.convert_to_int(event.attrib.get('id', None)),
+                'type': self.convert_to_str(event.attrib.get('type', None)),
+                'price': self.convert_to_bool(event.attrib.get('price', None)),
+                'kids': self.convert_to_bool(event.attrib.get('kids', None)),
+                'title': self.convert_to_str(event.find('title')),
+                'age_restricted': self.convert_to_str(event.find('age_restricted')),
+                'text': self.convert_to_str(event.find('text')),
+                'description': self.convert_to_str(event.find('description')),
+                'gallery': self.convert_to_list(event.find('gallery'), attr='href'),
+                'runtime': self.convert_to_int(event.find('runtime')),
+                'stage_theatre': self.convert_to_str(event.find('stage_theatre')),
+                'persons': self.convert_to_dict_in_list(event.find('persons')),
+                'tags': self.convert_to_list(event.find('tags')),
+            })
+
+    def parse_places(self, places):
+        for place in places:
+            self._places.append({
+                'uid': self.convert_to_int(place.attrib.get('id', None)),
+                'type': self.convert_to_str(place.attrib.get('type', None)),
+                'title': self.convert_to_str(place.find('title')),
+                'city': self.convert_to_str(place.find('city')),
+                'address': self.convert_to_str(place.find('address')),
+                # 'latitude': self.convert_to_float(place.find('coordinates').attrib['latitude']),
+                # 'longitude': self.convert_to_float(place.find('coordinates').attrib['longitude']),
+                'phones': self.convert_to_list(place.find('phones')),
+                'metros': self.convert_to_list(place.find('metros')),
+                'url': self.convert_to_str(place.find('url')),
+                'gallery': self.convert_to_list(place.find('gallery'), attr='href'),
+                'tags': self.convert_to_list(place.find('tags')),
+                'text': self.convert_to_str(place.find('text')),
+                'work_times': self.convert_to_list(place.find('work_times')),
+            })
+            coordinates = place.find('coordinates')
+            if coordinates:
+                self._places[-1]['latitude'] = self.convert_to_float(coordinates.attrib['latitude'])
+                self._places[-1]['longitude'] = self.convert_to_float(coordinates.attrib['longitude'])
+
+    def parse_schedule(self, schedule):
+        for session in schedule:
+            self._schedule.append({
+                'date': self.convert_to_date(session.attrib['date']),
+                'event': self.convert_to_int(session.attrib['event']),
+                'place': self.convert_to_int(session.attrib['place']),
+                'time': self.convert_to_str(session.attrib['time']),
+                'timetill': self.convert_to_str(session.attrib['timetill'])
+                    if session.attrib.get('timetill', None) else None,
+            })
+
+
+if __name__ == '__main__':
+    parser = XmlFeed('/home/anton/Projects/KudaGoTask/feed/test.xml')
+    parser.parse()
+    e = parser.get_events()
+    p = parser.get_places()
+    s = parser.get_schedule()
+    for ee in e:
+        print(ee, end='\n\n')
+    for pp in p:
+        print(pp, end='\n\n')
+    for ss in s:
+        print(ss, end='\n\n')
---
+++ parser/XmlParserBase.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+
+
+class XmlParserBase(object):
+    def __init__(self):
+        self.xml_data = {
+            'events': None,
+            'places': None,
+            'schedule': None
+        }
+
+        self._events = []
+        self._places = []
+        self._schedule = []
+
+    def parse(self):
+        self.parse_events(self.xml_data['events'])
+        self.parse_places(self.xml_data['places'])
+        self.parse_schedule(self.xml_data['schedule'])
+
+    def parse_events(self, events):
+        raise NotImplementedError
+
+    def parse_places(self, places):
+        raise NotImplementedError
+
+    def parse_schedule(self, schedule):
+        raise NotImplementedError
+
+    def get_events(self):
+        return self._events
+
+    def get_places(self):
+        return self._places
+
+    def get_schedule(self):
+        return self._schedule
+
+    @staticmethod
+    def get_text(x):
+        try:
+            return x.text
+        except AttributeError:
+            return x
+
+    @staticmethod
+    def convert_to_int(x):
+        x = XmlParserBase.get_text(x)
+        try:
+            return int(x)
+        except (ValueError, TypeError):
+            return None
+
+    @staticmethod
+    def convert_to_float(x):
+        x = XmlParserBase.get_text(x)
+        try:
+            return float(x)
+        except (ValueError, TypeError):
+            return None
+
+    @staticmethod
+    def convert_to_str(x):
+        x = XmlParserBase.get_text(x)
+        return str(x) if x is not None else x
+
+    @staticmethod
+    def convert_to_bool(x):
+        x = str(XmlParserBase.get_text(x)).lower()
+        if x in ('true', 'false'):
+            return (False, True)[x == 'true']
+        return None
+
+    @staticmethod
+    def convert_to_date(x):
+        try:
+            return datetime.strptime(x, '%Y-%m-%d')
+        except ValueError:
+            return None
+
+    @staticmethod
+    def convert_to_list(data, attr=None):
+        if data is None:
+            return []
+
+        res = []
+        for tag in data:
+            res.append(tag.attrib[attr] if attr else tag.text)
+
+        return res
+
+    @staticmethod
+    def convert_to_dict_in_list(data):
+        if data is None:
+            return []
+
+        res = []
+        for tag in data:
+            res.append({t.tag: t.text for t in tag})
+
+        return res
